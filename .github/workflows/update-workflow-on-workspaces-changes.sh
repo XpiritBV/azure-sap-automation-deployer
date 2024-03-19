8ac6f5ec1df724a57ca421170b05d33a179c5e97
Hindrik Bruinsma <hbruinsma@xs4some.nl>
2024-03-19 08:59:26 +0100

    Only mention deployer, as we use that as environment

--- .github/workflows/update-workflow-on-workspaces-changes.sh
+++ .github/workflows/update-workflow-on-workspaces-changes.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
  # Remove the current values
-yq -i 'del(.on.workflow_dispatch.inputs.deployer.options)' .github/workflows/01-deploy-control-plane.yaml
 yq -i 'del(.on.workflow_dispatch.inputs.library.options)' .github/workflows/01-deploy-control-plane.yaml
 
 # Add the new values
-for deployer in $(ls ${GITHUB_WORKSPACE}/WORKSPACES/DEPLOYER); do
-  yq -i '.on.workflow_dispatch.inputs.deployer.options += ["'${deployer}'"]' .github/workflows/01-deploy-control-plane.yaml
-done
-
 for library in $(ls ${GITHUB_WORKSPACE}/WORKSPACES/LIBRARY); do
   yq -i '.on.workflow_dispatch.inputs.library.options += ["'${library}'"]' .github/workflows/01-deploy-control-plane.yaml
 done
